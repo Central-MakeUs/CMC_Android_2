8fae321d4ff99edadf594a5009c70a1fc2a9be1a
JM2308 <wlals2987@naver.com>
2023-12-01 15:17:07 +0900

    Fix: Token 문제 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.cmc.android"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- app/src/main/java/com/cmc/android/utils/AuthorizationTokenInterceptor.kt
+++ app/src/main/java/com/cmc/android/utils/AuthorizationTokenInterceptor.kt
@@ -20,43 +20,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 class AuthorizationTokenInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder: Request.Builder = chain.request().newBuilder()
-        val response = chain.proceed(builder.build())
+        // val response = chain.proceed(builder.build())
 
         if (getAccessToken() != null) {
             builder.addHeader(Authorization_TOKEN, "${getAccessToken()}")
         }
 
-        if (response.code == 401) getNewToken()
+        return chain.proceed(builder.build())
 
-        return response
+//        if (response.code == 401) getNewToken()
+//
+//        return response
     }
-
-    private fun getNewToken() {
-        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
-        val getNewTokenInterface = retrofit.create(AuthInterface::class.java)
-        getNewTokenInterface.getNewToken(getRefreshToken().toString()).enqueue(object: Callback<ResponseWrapper<TokenResult>> {
-            override fun onResponse(call: Call<ResponseWrapper<TokenResult>>, response: retrofit2.Response<ResponseWrapper<TokenResult>>) {
-                if (response.code() == 200) {
-                    val tokenResponse = response.body()
-                    if (tokenResponse?.isSuccess == true) {
-                        var newAccessToken = tokenResponse.result!!.accessToken
-                        saveAccessToken(newAccessToken)
-                    } else {
-                        Log.d("TOKEN_ERROR", "Failure")
-                    }
-                } else {
-                    removeAccessToken()
-                    removeRefreshToken()
-                }
-            }
-            override fun onFailure(call: Call<ResponseWrapper<TokenResult>>, t: Throwable) {
-                Log.e("TOKEN_ERROR", "getNewToken-FAILURE", t)
-                removeAccessToken()
-                removeRefreshToken()
-            }
-        })
-    }
-}
 
 //    private fun getNewToken() {
 //        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
@@ -83,4 +58,4 @@ class AuthorizationTokenInterceptor: Interceptor {
 //            }
 //        })
 //    }
-//}
+}
